490c9c54e88ef85a4f4d84d4e8e82036f1d6607d
Raymond Chan <raymondchan@Raymonds-MacBook-Air.local>
2025-02-09 16:48:27 -0500

    add shopping and bought list api

---
+++ app/api/user/[id]/items/bought-list/route.tsx
@@ -0,0 +1,19 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+
+  const prisma = new PrismaClient();
+
+  const items = await prisma.item.findMany({
+    where: {
+      userId: id,
+      onList: false,
+    },
+  });
+
+  return Response.json(items);
+}
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,23 +1,45 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { TrashIcon, PencilSquareIcon } from "@heroicons/react/16/solid";
+import {
+  TrashIcon,
+  PencilSquareIcon,
+  ArrowUturnLeftIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/16/solid";
 
 export default function ListPage(req: any) {
   const userId = "1";
-  const [items, setItems] = useState([]);
+  const [shoppingItems, setShoppingItems] = useState([]);
+  const [boughtItems, setBoughtItems] = useState([]);
+  const [view, setView] = useState("SHOPPINGLIST");
 
   useEffect(() => {
-    fetch(`/api/user/${userId}/items`)
+    fetch(`/api/user/${userId}/items/shopping-list`)
       .then((res) => res.json())
       .then((data) => {
-        setItems(data);
+        setShoppingItems(data);
+      });
+
+    fetch(`/api/user/${userId}/items/bought-list`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBoughtItems(data);
       });
   }, []);
 
+  const items = view == "SHOPPINGLIST" ? shoppingItems : boughtItems;
+  const title = view == "SHOPPINGLIST" ? "Shopping List" : "Bought List";
+  const actionButton =
+    view == "SHOPPINGLIST" ? (
+      <CheckCircleIcon className="size-5 mr-2 text-green-500" />
+    ) : (
+      <ArrowUturnLeftIcon className="size-5 mr-2 text-black-500" />
+    );
+
   return (
     <div className="md:w-1/2 mx-auto">
-      <h1 className="text-4xl font-extrabold dark:text-white">Shopping List</h1>
+      <h1 className="text-4xl font-extrabold dark:text-white">{title}</h1>
       <ul id="todo-list">
         {items.map((item: any) => {
           const isChecked = !item.onList;
@@ -27,7 +49,7 @@ export default function ListPage(req: any) {
               key={item.id}
             >
               <label className="flex items-center">
-                <input type="checkbox" className="mr-2" checked={isChecked} />
+                {actionButton}
                 <span>{item.name}</span>
               </label>
               <div className="row-start-2 flex gap-6 flex-wrap items-center justify-center">
